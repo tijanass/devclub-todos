0415c5ac1af8fad59b43ae07b01d1cdb57f71319
tijanass <tijana.sokovic@outlook.com>
2020-01-18 21:03:31 +0100

    Updates

---
+++ precache-manifest.ce16b27695b620d8a2a45367dc68c704.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "922335f40a5d61f370cdece6d66ae4d4",
+    "url": "/devclub-todos/index.html"
+  },
+  {
+    "revision": "bcb9661ac9b2fdf74fb3",
+    "url": "/devclub-todos/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "b2d7f3f6dc4fd51bfc06",
+    "url": "/devclub-todos/static/css/main.7643c4a0.chunk.css"
+  },
+  {
+    "revision": "bcb9661ac9b2fdf74fb3",
+    "url": "/devclub-todos/static/js/2.a887e80e.chunk.js"
+  },
+  {
+    "revision": "f09321e517ab4567bb350ec8faae3b4d",
+    "url": "/devclub-todos/static/js/2.a887e80e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b2d7f3f6dc4fd51bfc06",
+    "url": "/devclub-todos/static/js/main.df64e49f.chunk.js"
+  },
+  {
+    "revision": "98f29d309d39c51dbc24",
+    "url": "/devclub-todos/static/js/runtime-main.5fec2a90.js"
+  }
+]);
